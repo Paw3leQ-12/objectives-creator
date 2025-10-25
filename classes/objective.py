a44d33ba08577313a392c95146edfe9b04578952
Lukasz <pijanowskilukasz1@gmail.com>
2025-10-25 16:46:48 +0200

    cleaner code and additional fixes

--- classes/objective.py
+++ classes/objective.py
@@ -1,25 +1,33 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLabel
 
 class Objective(QWidget):
-    def __init__(self):
+    def __init__(self, title, description):
         super().__init__()
+        objectiveInfo = QLabel(f"<h1 style='margin: 0;'>{title}</h1><p style='margin: 0;'>{description}</p>")
 
         buttonDelete = QPushButton()
         buttonDelete.setText("Delete")
         buttonDelete.setProperty("style", "danger")
+        buttonDelete.setFixedSize(130, 30)
+
         buttonEdit = QPushButton()
         buttonEdit.setText("Edit")
         buttonEdit.setProperty("style", "secondary")
+        buttonEdit.setFixedSize(130, 30)
+
         buttonFinish = QPushButton()
         buttonFinish.setText("Mark as done")
         buttonFinish.setProperty("style", "action")
+        buttonFinish.setFixedSize(130, 30)
+
         buttonSet = QPushButton()
         buttonSet.setText("Set as current")
         buttonSet.setProperty("style", "primary")
-
+        buttonSet.setFixedSize(130, 30)
 
         layout = QHBoxLayout()
         self.setLayout(layout)
+        layout.addWidget(objectiveInfo)
         layout.addWidget(buttonDelete)
         layout.addWidget(buttonEdit)
         layout.addWidget(buttonFinish)
--- main.py
+++ main.py
@@ -10,13 +10,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Objectives creator")
-        self.setMinimumHeight(400)
-        self.setMinimumWidth(600)
+        self.setMinimumHeight(800)
+        self.setMinimumWidth(1000)
         layout = QVBoxLayout()
         centralWidget = QWidget()
         centralWidget.setLayout(layout)
         self.setCentralWidget(centralWidget)
-        layout.addWidget(Objective())
+        layout.addWidget(Objective("Finish this app", "Just finish this app"))
 
 window = MainWindow()
 window.show()
